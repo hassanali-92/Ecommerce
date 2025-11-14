39db00d4734c5617535b987196db55f86a7c8b0e
hassanali-92 <hassan.programmer92@gmail.com>
2025-11-14 17:35:40 +0500

    Refactor RatingFilter component for clarity

--- src/components/Rating-filter.jsx
+++ src/components/Rating-filter.jsx
@@ -2,22 +2,18 @@ import Rating from "./Rating";
 
 const RatingFilter = ({ onChangeRating, selectedRating }) => {
   return (
-    <div className="h-48 p-5 cursor-pointer space-y-2  border border-gray-200 shadow-lg rounded-md">
+    <div className="h-48 p-5 cursor-pointer space-y-2 border border-gray-200 shadow-lg rounded-md">
       {[5, 4, 3, 2, 1].map((rating) => (
         <div
-          className="flex items-center gap-2 hover:opacity-80"
           key={rating}
+          className="flex items-center gap-2 hover:opacity-80"
           onClick={() => onChangeRating(rating)}
         >
           <Rating
-            stars={Array(5).fill(rating)}
-            ratingClassName={`${
-              rating == selectedRating ? " !text-[#ff3d47]" : ""
-            }`}
+            rating={rating}
+            ratingClassName={rating === selectedRating ? "selected" : ""}
           />
-          <p className="text-gray-400">
-            {rating === 5 ? 5.0 : rating.toFixed(1) + " +"}
-          </p>
+          <p className="text-gray-400">{rating} +</p>
         </div>
       ))}
     </div>
